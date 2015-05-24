194d961a7b6893f4b2fe4e3d1cba2ba7d7e10082
Ronen Moldovan <ronen.moldovan@gmail.com>
2015-05-24 18:03:15 +0300

    Updated descriotion

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'ronenm:offerjar-ui',
   version: '0.0.2',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Basic UI for OfferJar integration',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
